f4ed7f8bb517604c20396ac61a46f15a09f7d2f9
will3 <will3.git@gmail.com>
2018-06-19 13:47:14 +1200

    MINOR

--- src/migrations/addindexloctolistings.js
+++ src/migrations/addindexloctolistings.js
@@ -2,6 +2,5 @@ const connectDb = require('../db');
 
 connectDb().then((db) => {
   db.collection('listings').createIndex( { 'location' : '2dsphere' } );
-  db.collection('listingsTest').createIndex( { 'trademeId' : 1 } );
-  db.collection('listingsTest').createIndex( { 'location' : '2dsphere' } );
+  db.collection('listings').createIndex( { 'trademeId' : 1 } );
 });
